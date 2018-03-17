cb600e0207bf39a0528ab2ea6a8286c151aeef85
kheteshR <khetaram.rotangan@rapidqube.com>
2018-03-17 20:24:45 +0530

    node integrate

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    actions: {
+    placeClick:function(){
+        //console.log("hi manoj",showrecords);
+        //  swal("Way to Preclosure Request!", "", "success");
+
+         console.log("recordrecord")
+         this.transitionToRoute('place');
+        //  this.set('record',record)
+        //  this.transitionToRoute('requestpreclose')
+      }
+    },
     
 
 });
---
+++ app/controllers/place.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        process:function(){
+
+    var transactionstring={
+        
+        "profileObj" : {
+            "Name": "Risabh",
+        "FamilyPhysicianName":"Umesh",
+        "DOB":"22/12/1994",
+        "MedicalHistory": "ok",
+        "Address": "mumbai",
+        "GenderType": "male"
+          }
+  };
+  console.log("datastring"+JSON.stringify(transactionstring));
+  var mycontroller=this
+ 
+ // var token = sessionStorage.getItem('token');
+//   console.log("manoj",token);
+  return $.ajax({   
+  url:'http://192.168.1.26:8000/buildProfile',
+  type: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGF0dXMiOjIwMCwidXNlcnMiOnsiX2lkIjoiNWFhOTBiMzJlYTE3MTIzNGRlOGQyNjE0IiwicmVnaXN0ZXJPYmoiOnsiRmlyc3ROYW1lIjoicmlzYWJoIiwiTGFzdE5hbWUiOiJzaGFybWEiLCJQaG9uZSI6IjkxODM2OTk2NDU4MiJ9LCJuZW1faWQiOnsibmFtZSI6InJpc2FiaHNoYXJtYTcxQGdtYWlsLmNvbSIsImFjY291bnRzIjp7IjAiOnsiYnJhaW4iOnRydWUsImFsZ28iOiJwYXNzOmJpcDMyIiwiZW5jcnlwdGVkIjoiY2ViZmUyM2M0NjI5Yzc0NDc0Njc3ZGE1YTI0YzYyMTNmZWE5Zjc3NWY2OTQxYzFiZWY0ZjRjOTYwOTI1YjNkMGYxMjhmMGFiMmEzNzU5MDg2YWNiY2ExMzNhMzUxYTNmIiwiaXYiOiI3NjNmNmY2ZmI5MzgyODE2YzdjYjI2OGMwMzkwZWViNCIsImFkZHJlc3MiOiJNQlc2RkwyQlU0WTU1U1lEM1JTU1pNTUpJNzJXRklKTUlOTlQ2NVhXIiwibGFiZWwiOiJQcmltYXJ5IiwibmV0d29yayI6OTZ9fX0sIkVtYWlsIjoicmlzYWJoc2hhcm1hNzFAZ21haWwuY29tIiwiUGFzc3dvcmQiOiJwbVdrV1NCQ0w1MUJma2huNzl4UHVLQktIei8vSDZCK21ZNkc5L2VpZXVNPSIsIlR5cGUiOiJQYWl0ZW50IiwicmFwaWRJRCI6Imp2RVFFamxHUEE5QmxXTG9uR1BuUzEyaDZtdzdsNUhxOGZOTWtTUWJYRms9IiwicHJpdmF0ZUtleSI6IjI2Nzk3MWU0NDIwZjc0ODZiNzU5NzU4ZmFkNjQ3ODRlNTRkZWI0OWZhNjBjM2JiNzhiMTlmOGYwYzk0OTNhNDUiLCJvdHAiOjY2NjQsImVuY29kZWRNYWlsIjoiY21sellXSm9jMmhoY20xaE56RkFaMjFoYVd3dVkyOXQiLCJjcmVhdGVkX2F0IjoiV2VkIE1hciAxNCAyMDE4IDE3OjE0OjUwIEdNVCswNTMwIChJU1QpIiwiX192IjowLCJmYW1pbHlNZW1iZXJzIjpbXSwic3RhdHVzIjpbXX0sImlhdCI6MTUyMTAyNzk5OSwiZXhwIjo2MTUyMTAyNzk5OX0.Iw_twAG7j0hp2AWd_JxeaVQMEn89NQrYPLB4N3uJqzk"
+},
+  contentType: 'application/json',
+  data: JSON.stringify(transactionstring),
+  success: function(response) {
+  console.log(JSON.stringify(response));
+  var message = response.message;
+ 
+  mycontroller.set('message',message)
+  
+  console.log("message>>>>>>>>>>" + message);  
+  }
+
+})
+        }
+    }
+
+});
